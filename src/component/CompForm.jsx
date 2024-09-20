b7a743c648f5c1c00860dc9774baa632f42b53f4
Jholman Duvan Rojas Bautista <ing_jholmanrojas@hotmail.com>
2024-09-20 11:52:00 -0500

    new file:   src/component/CompForm.jsx modified:   src/component/CompSearch.jsx new file:   src/styles/CompForm.css modified:   src/styles/CompHeader.css modified:   src/styles/CompInsert.css modified:   src/styles/HomePage.css modified:   src/view/HomePage.jsx

---
+++ src/component/CompForm.jsx
@@ -0,0 +1,12 @@
+import '../styles/CompForm.css'
+import { useState } from 'react';
+
+
+function CompForm(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default CompForm;
--- src/component/CompSearch.jsx
+++ src/component/CompSearch.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 import '../styles/CompSearch.css'
 
-function CompSearch(){
+function CompSearch({recetas}){
 
     const [busqueda, setBusqueda] = useState('');
+
+    {/*const reetasFiltrados = reetas.filter(recetas =>
+        recetas.nombre.toLowerCase().includes(busqueda.toLowerCase())
+    );*/}
+    
     return(
         <form>
             <div className='comp-search'>
